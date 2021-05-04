bd1b25d1e1c6c0ebd9fcfadffcbe8c45892aeef6
andrew.tiema <andrew.tiema@ajiraconnect.com>
2021-05-04 09:37:12 +0300

    fix/hymn filter

--- app/src/main/java/com/example/hymn/api/ApiService.kt
+++ app/src/main/java/com/example/hymn/api/ApiService.kt
@@ -2,9 +2,18 @@ package com.example.hymn.api
 
 import com.example.hymn.model.Response
 import kotlinx.coroutines.Deferred
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
     @GET("songs")
     fun getSongsAsync() : Deferred<Response>
+
+    @FormUrlEncoded
+    @POST("search_hymn")
+    fun queryHymnsAsync(
+            @Field("title") title: String
+    ) : Deferred<Response>
 }
