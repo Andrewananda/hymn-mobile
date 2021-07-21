6e8697ecd20df163063fb9b20daf7afe2916a207
Andrew Tiema <andrewtiema@gmail.com>
2021-07-21 11:58:57 +0300

    fix/error layut

--- app/src/main/java/com/devstart/hymn/MainActivity.kt
+++ app/src/main/java/com/devstart/hymn/MainActivity.kt
@@ -65,6 +65,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.progressBar.show()
+        binding.errorLayout.hide()
         observeHymns()
         setUpAdapter()
     }
